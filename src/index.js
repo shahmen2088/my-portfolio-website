5bc7506620e3dc9882e668e79c0a51ec3a91cf2a
Shokhrukh <shoxrux20888@mail.ru>
2024-01-02 16:39:16 +0300

    feat: add form submission

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import "./img/Project3/travel__promo.png";
 import "./img/Project4/shop.png";
 import "./img/Project5/Auth.png";
 
-
 // Бургер меню
 
 const menu = document.querySelector("#menu");
@@ -59,4 +58,65 @@ function onScroll() {
 window.onload = function () {
   onScroll();
 };
+
+// Отправка формы
+
+function serializeForm(formNode) {
+  return new FormData(formNode);
+}
+
+async function sendData(data) {
+  return await fetch("/api/applay/", {
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: data,
+  });
+}
+
+function toggleLoader() {
+  const loader = document.getElementById("loader");
+  loader.classList.toggle("hidden");
+}
+
+function onSuccess(formNode) {
+  alert("Ваше обращение успешно отправлена!");
+  formNode.classList.toggle("hidden");
+}
+
+function onError(error) {
+  alert('Упс, что-то пошло не так :(');
+}
+
+function checkValidity(event) {
+  const formNode = event.target.form,
+ 
+  isValid = formNode.checkValidity();
+
+  console.log(formNode);
+
+  formNode.querySelector('button_form').disabled = !isValid
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const data = serializeForm(event.target);
+
+  toggleLoader();
+  const { status, error } = await sendData(data);
+  toggleLoader();
+
+  if (status === 200) {
+    onSuccess(event.target);
+  } else {
+    onError(error);
+  }
+}
+
+
+
+const applicantForm = document.getElementById("contact-form");
+
+// applicantForm.addEventListener('input', checkValidity);
+applicantForm.addEventListener("submit", handleFormSubmit);
 
